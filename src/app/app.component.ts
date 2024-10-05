f4c6d5b019bae0bfb5e9fc6b64fbb7d7a6dfca71
Tushar Mahalle <tusharmahalle0721@gmail.com>
2024-10-05 13:27:25 +0530

    Enabling Gh actions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title:string = 'Counter App';
+  title:string = 'counter-app';
 
   count:number = 0;
 
